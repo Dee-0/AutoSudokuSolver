20387bbefab8ff7712aa5f9f0393e4498c4c5b41
Dee <Deepository@hotmail.com>
2021-12-18 03:14:57 +0200

    Day 2, Algo made and working as intended, checks 3x3 box + Horizontal + Vertical lines, simple sudoku solver, need to clean code and add more visualization elements.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ Need to implement:
 [√]Screen
 [√]Grid
 [√]Input * NEED TO SAVE DATA TO SHOW ON SCREEN [√]
-[]Algorithm * Working on it NOW
-[]Visualized
+[√]Algorithm
+[]Visualized * Working on it NOW
 """
 
 import pygame
@@ -23,7 +23,9 @@ GREY = (128,128,128)
 BLACK = (0,0,0)
 GREEN = (0,255,0)
 TURQUOISE = (64,244,208)
+PINK = (255,102,255)
 
+done = False
 
 #Creating cell(box) object
 class Box:
@@ -37,6 +39,7 @@ class Box:
         self.total_rows = total_rows
         self.color = WHITE
         self.value = 0
+        self.options = []
 
     #Set value of cell
     def set_value(self,num):
@@ -61,6 +64,21 @@ class Box:
     def make_clear(self):
         self.color = WHITE
 
+    def is_clear(self):
+        if self.color == WHITE:
+            return True
+
+    def what_color(self):
+        return self.color
+
+    #Make it colored for visuals
+    def make_check_tile(self):
+        self.color = PINK
+
+    #Color cell as an option
+    def make_is_option(self):
+        self.color = GREY
+
     #Check if the cell IS clicked
     def is_clicked(self):
         return self.color
@@ -73,6 +91,61 @@ class Box:
         TextRect.center = (x, y)
         win.blit(TextSurf, TextRect)
 
+#Call to solve with a flag if done
+def solve(grid,win, ROWS, width):
+    global done
+    to_solve = 0
+    for row in grid:
+        for box in row:
+            if (box.get_value() == 0):
+                to_solve += 1
+                box.make_check_tile()
+                draw(win, grid, ROWS, width)
+                algorithm(grid,box, win, ROWS, width)
+                time.sleep(0.01)
+                box.make_clear()
+
+    if (to_solve == 0):
+        done = True
+
+#Algorithm
+def algorithm(grid,box, win, ROWS, width):
+    options = []
+    option = 0
+    current_box = []
+
+    for row in grid[box.row // 3 * 3:box.row // 3 * 3 + 3]:
+        for box in row[box.col // 3 * 3:box.col // 3 * 3 + 3]:
+            if (box.get_value() != 0):
+                current_box.append(box.get_value())
+
+    for num in range(1,10):
+        for row in grid[box.row // 3 * 3:box.row // 3 * 3 + 3]:
+            for box in row[box.col // 3 * 3:box.col // 3 * 3 + 3]:
+                if (box.get_value() == 0 and num not in current_box):
+                    if (check_horizontal(grid,box,num) is True and check_vertical(grid,box,num) is True):
+                        options.append(box)
+                        option = num
+        if (len(options) == 1):
+            options[0].set_value(option)
+        else:
+            options.clear()
+
+#Checking if the number exist Horizontally
+def check_horizontal(grid,cell,num):
+    for row in grid:
+        for box in row[cell.col:cell.col+1]:
+            if (box.get_value() == num):
+                return False
+    return True
+
+#Checking if the number exist veritcally
+def check_vertical(grid,cell,num):
+    for row in grid[cell.row:cell.row+1]:
+        for box in row:
+            if (box.get_value() == num):
+                return False
+    return True
 
 #Create the grid
 def make_grid(rows,width):
@@ -85,7 +158,6 @@ def make_grid(rows,width):
             grid[i].append(box)
 
     return grid
-
 
 #Draw the grid
 def draw_grid(win, rows, width):
@@ -114,10 +186,11 @@ def draw(win, grid, rows, width):
     draw_grid(win, rows, width)
     pygame.display.update()
 
+#Draw values on screen
 def draw_values(win, grid):
         for row in grid:
             for box in row:
-                if (box.get_value() in (1,2,3,4,5,6,7,8,9,)):
+                if (box.get_value() in (1,2,3,4,5,6,7,8,9)):
                     box.draw_number(win, str(box.get_value()), get_cell([box.x,box.y]))
 
 #Get clicked position
@@ -145,7 +218,6 @@ def text_objects(text, font):
     textSurface = font.render(text, True, BLACK)
     return textSurface, textSurface.get_rect()
 
-
 def main(win,width):
     ROWS = 9
     grid = make_grid(ROWS, width)
@@ -155,6 +227,7 @@ def main(win,width):
 
     while run:
         draw(win, grid, ROWS, width)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -173,26 +246,21 @@ def main(win,width):
                     draw(win, grid, ROWS, width)
                     tile_clicked = False
 
+            if pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                box = grid[row][col]
+                if (box.get_value() != 0):
+                    box.set_value(0)
+
             if event.type == pygame.KEYDOWN and tile_clicked:
-                clicked.set_value(int(event.unicode))
+                if (event.unicode in ('1','2','3','4','5','6','7','8','9')):
+                    clicked.set_value(int(event.unicode))
+                if event.key == pygame.K_SPACE:
+                    while (not done):
+                        solve(grid,win, ROWS, width)
 
 
     pygame.quit()
 
 main(WIN,WIDTH)
-
-
-
-
-#Junk that was tested
-
-"""#Receieve pressed number
-def get_inserted_number(key):
-    print(key)
-    event_key = key
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.event_key:
-                print("hi")"""
